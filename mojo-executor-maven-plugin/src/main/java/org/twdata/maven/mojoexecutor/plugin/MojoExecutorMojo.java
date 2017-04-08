bb1a975ce0f28359208879e3e6e4be14266f2ff9
Nick Cross <ncross@redhat.com>
2017-04-08 23:04:03 +0100

    Issue #25 Disable logging for executed mojo

--- mojo-executor-maven-plugin/src/main/java/org/twdata/maven/mojoexecutor/plugin/MojoExecutorMojo.java
+++ mojo-executor-maven-plugin/src/main/java/org/twdata/maven/mojoexecutor/plugin/MojoExecutorMojo.java
@@ -15,6 +15,9 @@
  */
 package org.twdata.maven.mojoexecutor.plugin;
 
+import org.apache.commons.lang.reflect.FieldUtils;
+import org.apache.maven.cli.logging.Slf4jConfiguration;
+import org.apache.maven.cli.logging.Slf4jConfigurationFactory;
 import org.apache.maven.execution.MavenSession;
 import org.apache.maven.model.Plugin;
 import org.apache.maven.plugin.AbstractMojo;
@@ -22,6 +25,9 @@ import org.apache.maven.plugin.BuildPluginManager;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.project.MavenProject;
 import org.codehaus.plexus.configuration.xml.XmlPlexusConfiguration;
+import org.codehaus.plexus.logging.Logger;
+import org.slf4j.ILoggerFactory;
+import org.slf4j.LoggerFactory;
 
 import static org.twdata.maven.mojoexecutor.MojoExecutor.executeMojo;
 import static org.twdata.maven.mojoexecutor.MojoExecutor.executionEnvironment;
@@ -83,13 +89,47 @@ public class MojoExecutorMojo extends AbstractMojo {
      */
     private BuildPluginManager pluginManager;
 
-    public void execute() throws MojoExecutionException {
-        if ( mavenProject.getArtifactId().equals( "mojo-executor-test-project-null-maven-project" ))
+    public void execute() throws MojoExecutionException
+    {
+        integrationTestSetup();
+
+        getLog().info( "Executing with maven project " + mavenProject + " for session " + mavenSession );
+        executeMojo( plugin, goal, toXpp3Dom( configuration ),
+                     executionEnvironment( mavenProject, mavenSession, pluginManager ) );
+    }
+
+    /**
+     * Used purely to customise the setups for the integration tests.
+     *
+     * @throws MojoExecutionException
+     */
+    private void integrationTestSetup() throws MojoExecutionException
+    {
+        // Test specific conditionals...
+        if ( mavenProject.getArtifactId().equals( "mojo-executor-test-project-quiet" ) )
+        {
+            // Maven < 3.1
+            Logger logger;
+            try
+            {
+                logger = (Logger) FieldUtils.readField( getLog(), "logger", true );
+            }
+            catch ( IllegalAccessException e )
+            {
+                throw new MojoExecutionException( "Unable to access logger field ", e );
+            }
+            logger.setThreshold( 5 );
+
+            // Maven >= 3.1
+            ILoggerFactory slf4jLoggerFactory = LoggerFactory.getILoggerFactory();
+            Slf4jConfiguration slf4jConfiguration = Slf4jConfigurationFactory.getConfiguration( slf4jLoggerFactory );
+            slf4jConfiguration.setRootLoggerLevel( Slf4jConfiguration.Level.ERROR);
+            slf4jConfiguration.activate();
+        }
+        else if ( mavenProject.getArtifactId().equals( "mojo-executor-test-project-null-maven-project" ) )
         {
             mavenProject = null;
         }
-        getLog().info( "Executing with maven project " + mavenProject + " for session " + mavenSession );
-        executeMojo(plugin, goal, toXpp3Dom(configuration),
-                executionEnvironment(mavenProject, mavenSession, pluginManager));
+
     }
 }
